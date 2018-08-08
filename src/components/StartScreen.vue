54f707b3fb6b1e928d08c25cc080d0a1e34b792b
Shokin Ilya <atlus3@mail.ru>
2018-08-08 08:26:30 +0300

    init2

---
+++ src/components/StartScreen.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="alert alert-secondary">
+		<h3>Are you ready?</h3>
+		<button class="btn btn-success" v-on:click="$emit('onStart')">Start</button>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style scoped>
+	
+</style>
